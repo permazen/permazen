25098a947023724e2126c05b0d5dd29f09ba7b2a
Archie L. Cobbs <archiecobbs@users.noreply.github.com>
2012-02-28 21:49:05 +0000

    Add Pattern validation constraint that works on any object via toString().

---
+++ src/java/org/dellroad/stuff/validation/Pattern.java
@@ -0,0 +1,48 @@
+
+/*
+ * Copyright (C) 2011 Archie L. Cobbs. All rights reserved.
+ *
+ * $Id$
+ */
+
+package org.dellroad.stuff.validation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import javax.validation.ReportAsSingleViolation;
+
+/**
+ * Works like the standard {@link javax.validation.constraints.Pattern @Pattern} but applies to any
+ * type of object, converting to {@link String} as necessary via {@link Object#toString}, and recursing
+ * on collection types.
+ */
+@Documented
+@Constraint(validatedBy = PatternValidator.class)
+@Target({ ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+@ReportAsSingleViolation
+public @interface Pattern {
+
+    String message() default "Does not match the pattern \"{regex}\"";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+    /**
+     * Regular expression that must be matched.
+     */
+    String regexp();
+
+    /**
+     * Regular expression flags.
+     */
+    javax.validation.constraints.Pattern.Flag[] flags() default {};
+}
+
---
+++ src/java/org/dellroad/stuff/validation/PatternValidator.java
@@ -0,0 +1,58 @@
+
+/*
+ * Copyright (C) 2011 Archie L. Cobbs. All rights reserved.
+ *
+ * $Id$
+ */
+
+package org.dellroad.stuff.validation;
+
+import java.util.Collection;
+import java.util.regex.PatternSyntaxException;
+
+import javax.validation.ConstraintDeclarationException;
+import javax.validation.ConstraintValidatorContext;
+
+/**
+ * Validator for the @{@link Pattern} constraint.
+ *
+ * @see Sorted
+ */
+public class PatternValidator extends AbstractValidator<Pattern, Object> {
+
+    private java.util.regex.Pattern pattern;
+
+    @Override
+    public void initialize(Pattern pattern) {
+        super.initialize(pattern);
+
+        // Get flags
+        int flags = 0;
+        for (javax.validation.constraints.Pattern.Flag flag : this.annotation.flags())
+            flags |= flag.getValue();
+
+        // Compile regular expression
+        try {
+            this.pattern = java.util.regex.Pattern.compile(this.annotation.regexp(), flags);
+        } catch (PatternSyntaxException e) {
+            throw new ConstraintDeclarationException("Invalid regular expression `" + annotation.regexp() + "': " + e.getMessage());
+        }
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public boolean isValid(Object value, ConstraintValidatorContext context) {
+
+        // Handle null
+        if (value == null)
+            return true;
+
+        // Handle collection
+        if (value instanceof Collection)
+            return this.isCollectionValid((Collection<?>)value, context);
+
+        // Check pattern
+        return this.pattern.matcher(value.toString()).matches();
+    }
+}
+
