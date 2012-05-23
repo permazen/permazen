0884c575bc248537fda85b9f23f9f8218e7d26a8
Archie L. Cobbs <archiecobbs@users.noreply.github.com>
2012-05-23 17:45:14 +0000

    Add SimpleKeyedContainer and AbstractSimpleContainer superclass.

--- src/java/org/dellroad/stuff/vaadin/SimpleContainer.java
+++ src/java/org/dellroad/stuff/vaadin/SimpleContainer.java
@@ -7,293 +7,63 @@
 
 package org.dellroad.stuff.vaadin;
 
-import com.vaadin.data.Container;
-import com.vaadin.data.Item;
-import com.vaadin.data.Property;
-import com.vaadin.data.util.AbstractInMemoryContainer;
-import com.vaadin.data.util.DefaultItemSorter;
-import com.vaadin.data.util.filter.SimpleStringFilter;
-import com.vaadin.data.util.filter.UnsupportedFilterException;
-
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
 
 /**
- * Simple read-only, in-memory {@link Container} implementation where each {@link Item} is backed by a Java object.
- *
- * <p>
- * The exposed properties are defined via {@link PropertyDef}s, and a {@link PropertyExtractor} is used to
- * actually extract the property values from each underlying object.
+ * An {@link AbstractSimpleContainer} with {@link Integer} item IDs.
  *
  * @param <T> the type of the Java objects that back each {@link Item} in the container
- * @see SimpleItem
- * @see SimpleProperty
+ * @see AbstractSimpleContainer
  */
 @SuppressWarnings("serial")
-public class SimpleContainer<T> extends AbstractInMemoryContainer<Integer, String, SimpleItem<T>>
-  implements Container.Filterable, Container.SimpleFilterable, Container.Sortable {
-
-    private final HashMap<String, PropertyDef<?>> propertyMap = new HashMap<String, PropertyDef<?>>();
-    private final PropertyExtractor<? super T> propertyExtractor;
+public class SimpleContainer<T> extends AbstractSimpleContainer<Integer, T> {
 
     private ArrayList<T> items = new ArrayList<T>(0);
-
-// Constructor
 
     /**
      * Constructor.
      *
+     * <p>
+     * After using this constructor, a subsequent invocation of {@link #setProperties setProperties()} is required
+     * to define the properties of this container.
+     *
      * @param propertyExtractor used to extract properties from the underlying Java objects
-     * @param propertyDefs container property definitions
      * @throws IllegalArgumentException if {@code propertyExtractor} is null
      */
-    public SimpleContainer(PropertyExtractor<? super T> propertyExtractor, Collection<? extends PropertyDef<?>> propertyDefs) {
-        if (propertyExtractor == null)
-            throw new IllegalArgumentException("null extractor");
-        this.propertyExtractor = propertyExtractor;
-        this.setProperties(propertyDefs);
-        this.setItemSorter(new SimpleItemSorter());
+    public SimpleContainer(PropertyExtractor<? super T> propertyExtractor) {
+        super(propertyExtractor);
     }
 
-// Public methods
-
     /**
-     * Change the configured properties of this container.
+     * Primary constructor.
      *
+     * @param propertyExtractor used to extract properties from the underlying Java objects
      * @param propertyDefs container property definitions
-     * @throws IllegalArgumentException if {@code propertyDefs} is null
+     * @throws IllegalArgumentException if either parameter is null
      */
-    public void setProperties(Collection<? extends PropertyDef<?>> propertyDefs) {
-        if (propertyDefs == null)
-            throw new IllegalArgumentException("null propertyDefs");
-        this.propertyMap.clear();
-        for (PropertyDef<?> propertyDef : propertyDefs)
-            this.propertyMap.put(propertyDef.getName(), propertyDef);
-        this.fireContainerPropertySetChange();
+    public SimpleContainer(PropertyExtractor<? super T> propertyExtractor, Collection<? extends PropertyDef<?>> propertyDefs) {
+        super(propertyExtractor, propertyDefs);
     }
 
-    /**
-     * Change this container's contents.
-     *
-     * @param contents new container contents
-     * @throws IllegalArgumentException if {@code contents} or any item in {@code contents} is null
-     */
-    public void load(Iterable<? extends T> contents) {
-
-        // Sanity check
-        if (contents == null)
-            throw new IllegalArgumentException("null contents");
-
-        // Bulk load and register items with id's 0, 1, 2, ...
-        this.items = new ArrayList<T>();
-        int index = 0;
-        for (T obj : contents) {
-            if (obj == null)
-                throw new IllegalArgumentException("null item in contents at index " + this.items.size());
-            SimpleItem<T> item = new SimpleItem<T>(obj, this.propertyMap, this.propertyExtractor);
-            this.internalAddItemAtEnd(index++, item, false);
-            this.items.add(obj);
-        }
-
-        // Apply filters
-        this.filterAll();
-
-        // Notify subclass
-        this.afterReload();
-
-        // Fire event
-        this.fireItemSetChange();
-    }
-
-    /**
-     * Return the number of items in this container. This includes items that have been filtered out.
-     */
-    public int size() {
-        return this.getAllItemIds().size();
-    }
-
-    /**
-     * Get the underlying Java object corresponding to the given item ID.
-     * This method ignores any filtering (i.e., filtered-out objects are still accessible).
-     *
-     * @param itemId item ID
-     * @return the corresponding Java object, or null if not found
-     */
+    @Override
     public T getJavaObject(Object itemId) {
         if (!(itemId instanceof Integer))
             return null;
-        return this.getJavaObject((int)(Integer)itemId);
-    }
-
-    /**
-     * Get the underlying Java object corresponding to the given item ID.
-     * This method ignores any filtering (i.e., filtered-out objects are still accessible).
-     *
-     * @param index item ID
-     * @return the corresponding Java object, or null if not found
-     */
-    public T getJavaObject(int index) {
+        int index = ((Integer)itemId).intValue();
         if (index < 0 || index >= this.items.size())
             return null;
         return this.items.get(index);
     }
 
-    /**
-     * Get the container item ID corresponding to the given underlying Java object which is wrapped by this container.
-     * Objects are tested for equality using {@link Object#equals Object.equals()}.
-     *
-     * <p>
-     * Note: this method performs a linear search of the container.
-     *
-     * @return item ID corresponding to {@code object}, or null if {@code object} is not found in this container
-     * @throws IllegalArgumentException if {@code object} is null
-     * @see #getItemIdForSame
-     */
-    public Integer getItemIdFor(Object object) {
-        if (object == null)
-            throw new IllegalArgumentException("null object");
-        for (int i = 0; i < this.items.size(); i++) {
-            if (object.equals(this.items.get(i)))
-                return i;
-        }
-        return null;
-    }
-
-    /**
-     * Get the container item ID corresponding to the given underlying Java object which is wrapped by this container.
-     * Objects are tested for equality using object equality, not {@link Object#equals Object.equals()}.
-     *
-     * <p>
-     * Note: this method performs a linear search of the container.
-     *
-     * @return item ID corresponding to {@code object}, or null if {@code object} is not found in this container
-     * @throws IllegalArgumentException if {@code object} is null
-     * @see #getItemIdFor
-     */
-    public Integer getItemIdForSame(Object object) {
-        if (object == null)
-            throw new IllegalArgumentException("null object");
-        for (int i = 0; i < this.items.size(); i++) {
-            if (this.items.get(i) == object)
-                return i;
-        }
-        return null;
-    }
-
-// Container and superclass required methods
-
     @Override
-    public Set<String> getContainerPropertyIds() {
-        return Collections.unmodifiableSet(this.propertyMap.keySet());
+    protected void resetItemIds() {
+        this.items = new ArrayList<T>();
     }
 
     @Override
-    public Property getContainerProperty(Object itemId, Object propertyId) {        // TODO: VAADIN7
-        SimpleItem<T> entityItem = this.getItem(itemId);
-        if (entityItem == null)
-            return null;
-        return entityItem.getItemProperty(propertyId);
-    }
-
-    @Override
-    public Class<?> getType(Object propertyId) {
-        PropertyDef<?> propertyDef = this.propertyMap.get(propertyId);
-        return propertyDef != null ? propertyDef.getType() : null;
-    }
-
-    @Override
-    public SimpleItem<T> getUnfilteredItem(Object itemId) {
-        if (!(itemId instanceof Integer))
-            return null;
-        int index = (Integer)itemId;
-        if (index < 0 || index >= this.items.size())
-            return null;
-        return new SimpleItem<T>(this.items.get(index), this.propertyMap, this.propertyExtractor);
-    }
-
-// Subclass methods
-
-    /**
-     * Subclass hook invoked after each reload but prior to invoking {@link #fireItemSetChange}.
-     *
-     * <p>
-     * The implementation in {@link SimpleContainer} does nothing.
-     */
-    protected void afterReload() {
-    }
-
- // Container methods
-
-    @Override
-    public void sort(Object[] propertyId, boolean[] ascending) {
-        super.sortContainer(propertyId, ascending);
-    }
-
-    @Override
-    public void addContainerFilter(Object propertyId, String filterString, boolean ignoreCase, boolean onlyMatchPrefix) {
-        try {
-            this.addFilter(new SimpleStringFilter(propertyId, filterString, ignoreCase, onlyMatchPrefix));
-        } catch (UnsupportedFilterException e) {
-            // the filter instance created here is always valid for in-memory containers
-            throw new RuntimeException("unexpected exception", e);
-        }
-    }
-
-    @Override
-    public void removeAllContainerFilters() {
-        this.removeAllFilters();
-    }
-
-    @Override
-    public void removeContainerFilters(Object propertyId) {
-        this.removeFilters(propertyId);
-    }
-
-    @Override
-    public void addContainerFilter(Filter filter) {
-        this.addFilter(filter);
-    }
-
-    @Override
-    public void removeContainerFilter(Filter filter) {
-        this.removeFilter(filter);
-    }
-
-    @Override
-    public Collection<?> getSortableContainerPropertyIds() {
-        ArrayList<String> propertyIds = new ArrayList<String>(this.propertyMap.size());
-        for (Map.Entry<String, PropertyDef<?>> entry : this.propertyMap.entrySet()) {
-            if (entry.getValue().isSortable())
-                propertyIds.add(entry.getKey());
-        }
-        return propertyIds;
-    }
-
-// ItemSorter class
-
-    /**
-     * {@link ItemSorter} implementation used by {@link SimpleContainer}.
-     */
-    private class SimpleItemSorter extends DefaultItemSorter {
-
-        @Override
-        @SuppressWarnings("unchecked")
-        protected int compareProperty(Object propertyId, boolean ascending, Item item1, Item item2) {
-            PropertyDef<?> propertyDef = SimpleContainer.this.propertyMap.get(propertyId);
-            if (propertyDef == null || !propertyDef.isSortable())
-                return super.compareProperty(propertyId, ascending, item1, item2);
-            int diff = this.sort(propertyDef, item1, item2);
-            return ascending ? diff : -diff;
-        }
-
-        // This method exists only to allow the generic parameter <V> to be bound
-        private <V> int sort(PropertyDef<V> propertyDef, Item item1, Item item2) {
-            return propertyDef.sort(propertyDef.read(item1), propertyDef.read(item2));
-        }
+    protected Integer generateItemId(T obj) {
+        return this.items.size();
     }
 }
 
---
+++ src/java/org/dellroad/stuff/vaadin/SimpleKeyedContainer.java
@@ -0,0 +1,130 @@
+
+/*
+ * Copyright (C) 2011 Archie L. Cobbs. All rights reserved.
+ *
+ * $Id$
+ */
+
+package org.dellroad.stuff.vaadin;
+
+import java.util.Collection;
+import java.util.HashMap;
+
+/**
+ * An {@link AbstractSimpleContainer} where the item IDs are generated from the items themselves
+ * by the subclass-provided method {@link #getKeyFor}.
+ *
+ * <p>
+ * Restriction: instances can never contain two objects whose keys are equal (in the sense of {@link Object#equals}).
+ *
+ * @param <I> the item ID type
+ * @param <T> the type of the Java objects that back each {@link Item} in the container
+ * @see AbstractSimpleContainer
+ */
+@SuppressWarnings("serial")
+public abstract class SimpleKeyedContainer<I, T> extends AbstractSimpleContainer<I, T> {
+
+    private HashMap<I, T> objectMap = new HashMap<I, T>(0);
+
+    /**
+     * Constructor.
+     *
+     * <p>
+     * After using this constructor, a subsequent invocation of {@link #setProperties setProperties()} is required
+     * to define the properties of this container.
+     *
+     * @param propertyExtractor used to extract properties from the underlying Java objects
+     * @throws IllegalArgumentException if {@code propertyExtractor} is null
+     */
+    public SimpleKeyedContainer(PropertyExtractor<? super T> propertyExtractor) {
+        super(propertyExtractor);
+    }
+
+    /**
+     * Primary constructor.
+     *
+     * @param propertyExtractor used to extract properties from the underlying Java objects
+     * @param propertyDefs container property definitions
+     * @throws IllegalArgumentException if either parameter is null
+     */
+    public SimpleKeyedContainer(PropertyExtractor<? super T> propertyExtractor, Collection<? extends PropertyDef<?>> propertyDefs) {
+        super(propertyExtractor, propertyDefs);
+    }
+
+    @Override
+    public T getJavaObject(Object itemId) {
+        return this.objectMap.get(itemId);
+    }
+
+    /**
+     * Get the container item ID corresponding to the given underlying Java object which is wrapped by this container.
+     * Objects are tested for equality using {@link Object#equals Object.equals()}.
+     *
+     * <p>
+     * This method uses an internal hash map for efficiency, and assumes that two underlying container objects that
+     * are {@linkplain Object#equal equal} will have the same {@linkplain #getKeyFor key}.
+     *
+     * @param obj underlying container object
+     * @return item ID corresponding to {@code object}, or null if {@code object} is not found in this container
+     * @throws IllegalArgumentException if {@code object} is null
+     * @see #getItemIdForSame
+     */
+    public I getItemIdFor(T obj) {
+        if (obj == null)
+            throw new IllegalArgumentException("null object");
+        I key = this.getKeyFor(obj);
+        if (key == null)
+            throw new IllegalArgumentException("null key returned by getKeyFor() for object " + obj);
+        T candidate = this.objectMap.get(key);
+        return obj.equals(candidate) ? key : null;
+    }
+
+    /**
+     * Get the container item ID corresponding to the given underlying Java object which is wrapped by this container.
+     * Objects are tested for equality using object equality, not {@link Object#equals Object.equals()}.
+     *
+     * <p>
+     * This method uses an internal hash map for efficiency.
+     *
+     * @param obj underlying container object
+     * @return item ID corresponding to {@code object}, or null if {@code object} is not found in this container
+     * @throws IllegalArgumentException if {@code object} is null
+     * @see #getItemIdFor
+     */
+    public I getItemIdForSame(T obj) {
+        if (obj == null)
+            throw new IllegalArgumentException("null object");
+        I key = this.getKeyFor(obj);
+        if (key == null)
+            throw new RuntimeException("null key returned by getKeyFor() for object " + obj);
+        T candidate = this.objectMap.get(key);
+        return obj == candidate ? key : null;
+    }
+
+    @Override
+    protected void resetItemIds() {
+        this.objectMap = new HashMap<I, T>();
+    }
+
+    @Override
+    protected final I generateItemId(T obj) {
+        I key = this.getKeyFor(obj);
+        if (key == null)
+            throw new RuntimeException("null key returned by getKeyFor() for object " + obj);
+        T previous = this.objectMap.put(key, obj);
+        if (previous != null)
+            throw new RuntimeException("same key " + key + " used by two different objects " + previous + " and " + obj);
+        return key;
+    }
+
+    /**
+     * Get the key to be used as item ID for the given object.
+     * All objects in the container must have unique keys.
+     * This method must return the same key for the same object even if invoked multiple times.
+     *
+     * @param obj underlying container object
+     * @return key for object
+     */
+    protected abstract I getKeyFor(T obj);
+}
+
