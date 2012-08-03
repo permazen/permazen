f0ef9fa2a71f4e593f48a0bb5d291fdb4eaf0d66
Archie L. Cobbs <archiecobbs@users.noreply.github.com>
2012-08-03 22:11:53 +0000

    Add a bean that exposes the containing application context.

---
+++ src/java/org/dellroad/stuff/spring/ApplicationContextBean.java
@@ -0,0 +1,17 @@
+
+/*
+ * Copyright (C) 2011 Archie L. Cobbs. All rights reserved.
+ *
+ * $Id$
+ */
+
+package org.dellroad.stuff.spring;
+
+import org.springframework.context.support.ApplicationObjectSupport;
+
+/**
+ * Bean that exposes the containing application context via the {@link #getApplicationContext} property.
+ */
+public class ApplicationContextBean extends ApplicationObjectSupport {
+}
+
