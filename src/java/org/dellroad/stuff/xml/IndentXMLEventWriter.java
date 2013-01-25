db48ba1887fda2f77271df5269a040c405d90af2
Archie L. Cobbs <archiecobbs@users.noreply.github.com>
2013-01-25 15:56:07 +0000

    Add IndentXMLEventWriter and StackXMLEventReader.

---
+++ src/java/org/dellroad/stuff/xml/IndentXMLEventWriter.java
@@ -0,0 +1,150 @@
+
+/*
+ * Copyright (C) 2012 Archie L. Cobbs. All rights reserved.
+ *
+ * $Id$
+ */
+
+package org.dellroad.stuff.xml;
+
+import java.util.Arrays;
+
+import javax.xml.namespace.NamespaceContext;
+import javax.xml.stream.XMLEventFactory;
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLEventWriter;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.XMLEvent;
+
+/**
+ * Wrapper for an underlying {@link XMLEventWriter} that automatically adds indentation to the event stream.
+ */
+public class IndentXMLEventWriter implements XMLEventWriter {
+
+    private final XMLEventWriter writer;
+    private final XMLEventFactory factory;
+    private final int indent;
+
+    private int lastEvent = -1;
+    private int depth;
+
+    /**
+     * Constructor.
+     *
+     * @param writer underlying writer
+     * @param factory event factory
+     * @param indent indent amount, or negative to not add any whitespace
+     * @throws IllegalArgumentException if {@code writer} or {@code factory} is null
+     */
+    public IndentXMLEventWriter(XMLEventWriter writer, XMLEventFactory factory, int indent) {
+        if (writer == null)
+            throw new IllegalArgumentException("null writer");
+        if (factory == null)
+            throw new IllegalArgumentException("null factory");
+        this.writer = writer;
+        this.factory = factory;
+        this.indent = indent;
+    }
+
+    /**
+     * Convenience constructor. Equivalent to:
+     * <blockquote>
+     *  {@link #IndentXMLEventWriter(XMLEventWriter, XMLEventFactory, int)
+     *      IndentXMLEventWriter(writer, XMLEventFactory.newFactory(), indent)}
+     * </blockquote>
+     *
+     * @param writer underlying writer
+     * @param indent indent amount, or negative to not add any whitespace
+     * @throws IllegalArgumentException if {@code writer} or {@code factory} is null
+     */
+    public IndentXMLEventWriter(XMLEventWriter writer, int indent) {
+        this(writer, XMLEventFactory.newFactory(), indent);
+    }
+
+    @Override
+    public void add(XMLEvent event) throws XMLStreamException {
+        switch (event.getEventType()) {
+        case XMLEvent.START_ELEMENT:
+            switch (lastEvent) {
+            case XMLEvent.START_ELEMENT:
+            case XMLEvent.END_ELEMENT:
+                this.indent(this.depth);
+                break;
+            default:
+                break;
+            }
+            this.depth++;
+            break;
+        case XMLEvent.END_ELEMENT:
+            this.depth--;
+            switch (lastEvent) {
+            case XMLEvent.START_ELEMENT:
+                break;
+            case XMLEvent.END_ELEMENT:
+                this.indent(this.depth);
+                break;
+            default:
+                break;
+            }
+            break;
+        default:
+            break;
+        }
+        this.lastEvent = event.getEventType();
+        this.writer.add(event);
+    }
+
+    @Override
+    public void flush() throws XMLStreamException {
+        this.writer.flush();
+    }
+
+    @Override
+    public void close() throws XMLStreamException {
+        this.writer.close();
+    }
+
+    @Override
+    public void add(XMLEventReader reader) throws XMLStreamException {
+        while (reader.hasNext())
+            this.add(reader.nextEvent());
+    }
+
+    @Override
+    public String getPrefix(String uri) throws XMLStreamException {
+        return this.writer.getPrefix(uri);
+    }
+
+    @Override
+    public void setPrefix(String prefix, String uri) throws XMLStreamException {
+        this.writer.setPrefix(prefix, uri);
+    }
+
+    @Override
+    public void setDefaultNamespace(String uri) throws XMLStreamException {
+        this.writer.setDefaultNamespace(uri);
+    }
+
+    @Override
+    public void setNamespaceContext(NamespaceContext context) throws XMLStreamException {
+        this.writer.setNamespaceContext(context);
+    }
+
+    @Override
+    public NamespaceContext getNamespaceContext() {
+        return this.writer.getNamespaceContext();
+    }
+
+    /**
+     * Emit a newline followed by indentation to the given depth.
+     */
+    protected void indent(int depth) throws XMLStreamException {
+        if (this.indent < 0)
+            return;
+        char[] buf = new char[1 + depth * this.indent];
+        Arrays.fill(buf, ' ');
+        buf[0] = '\n';
+        this.writer.add(this.factory.createIgnorableSpace(new String(buf)));
+    }
+}
+
---
+++ src/java/org/dellroad/stuff/xml/StackXMLEventReader.java
@@ -0,0 +1,102 @@
+
+/*
+ * Copyright (C) 2012 Archie L. Cobbs. All rights reserved.
+ *
+ * $Id$
+ */
+
+package org.dellroad.stuff.xml;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.StartElement;
+import javax.xml.stream.events.XMLEvent;
+import javax.xml.stream.util.EventReaderDelegate;
+
+/**
+ * Wrapper for a {@link XMLEventReader} that keeps track of the current element(s) being parsed as a stack
+ * of {@link StartElement} events.
+ *
+ * <p>
+ * Each time a {@link StartElement} is read from the underlying {@link XMLEventReader}, it is pushed onto
+ * the stack; each time a {@link javax.xml.stream.events.EndElement} is read from the underlying {@link XMLEventReader},
+ * the previously push {@link StartElement} is popped off.
+ * </p>
+ *
+ * <p>
+ * Invocations of {@link #peek} do not affect the element stack.
+ * </p>
+ */
+public class StackXMLEventReader extends EventReaderDelegate {
+
+    private final ArrayList<StartElement> stack = new ArrayList<StartElement>();
+
+    /**
+     * Constructor.
+     *
+     * @param reader underlying reader
+     * @throws IllegalArgumentException if {@code reader} is null
+     */
+    public StackXMLEventReader(XMLEventReader reader) {
+        super(reader);
+        if (reader == null)
+            throw new IllegalArgumentException("null reader");
+    }
+
+    /**
+     * Get the top-most {@link StartElement} event on the stack. This corresponds to the innermost XML
+     * element currently being read.
+     *
+     * @return current element, or null if the current parse position is outside of the document element
+     */
+    public StartElement getTopElement() {
+        return !this.stack.isEmpty() ? this.stack.get(this.stack.size() - 1) : null;
+    }
+
+    /**
+     * Get the entire stack of {@link StartElement}s.
+     *
+     * <p>
+     * A copy is returned; changes to the returned list do not affect this instance.
+     * </p>
+     *
+     * @return element stack; the first element in the list is the document element
+     */
+    public List<StartElement> getElementStack() {
+        return new ArrayList<StartElement>(this.stack);
+    }
+
+    @Override
+    public XMLEvent nextEvent() throws XMLStreamException {
+        return this.adjust(super.nextEvent());
+    }
+
+    @Override
+    public XMLEvent nextTag() throws XMLStreamException {
+        return this.adjust(super.nextTag());
+    }
+
+    /**
+     * Adjust the stack after reading the given event.
+     *
+     * @param event newly read event
+     * @return {@code event}
+     */
+    protected XMLEvent adjust(XMLEvent event) {
+        switch (event.getEventType()) {
+        case XMLEvent.START_ELEMENT:
+            this.stack.add(event.asStartElement());
+            break;
+        case XMLEvent.END_ELEMENT:
+            this.stack.remove(this.stack.size() - 1);
+            break;
+        default:
+            break;
+        }
+        return event;
+    }
+}
+
