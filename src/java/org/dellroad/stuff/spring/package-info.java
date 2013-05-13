118fa779367d6e2119b7831f5ea24d3fe78cb73f
Archie L. Cobbs <archiecobbs@users.noreply.github.com>
2013-05-13 16:32:59 +0000

    Remove @PostCompletionSupport and related classes. Spring's TransactionSynchronizationManager provides equivalent functionality.

--- src/java/org/dellroad/stuff/spring/package-info.java
+++ src/java/org/dellroad/stuff/spring/package-info.java
@@ -9,16 +9,13 @@
  * <a href="http://www.springframework.org/">Spring</a> related classes.
  *
  * <p>
- * This package implements the <code>&lt;dellroad-stuff:sql&gt;</code> and <code>&lt;dellroad-stuff:sql-update&gt;</code>
+ * This also package implements the <code>&lt;dellroad-stuff:sql&gt;</code> and <code>&lt;dellroad-stuff:sql-update&gt;</code>
  * custom Spring XML elements; these define beans of type {@link org.dellroad.stuff.schema.SQLCommandList SQLCommandList} and
  * {@link org.dellroad.stuff.schema.SpringSQLSchemaUpdate SpringSQLSchemaUpdate}, respectively.
- *
- * <p>
- * This package also implements the <code>&lt;dellroad-stuff:post-completion&gt;</code> custom Spring XML element.
+ * </p>
  *
  * @see org.dellroad.stuff.schema.SQLCommandList
  * @see org.dellroad.stuff.schema.SpringSQLSchemaUpdate
- * @see org.dellroad.stuff.spring.PostCompletionSupport
  * @see <a href="http://www.springframework.org/">Spring Framework</a>
  */
 package org.dellroad.stuff.spring;
