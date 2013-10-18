9fb3b74f54dae803b46299b64b0f4655461b2058
Archie L. Cobbs <archiecobbs@users.noreply.github.com>
2013-10-18 20:35:34 +0000

    Fix glitch in prependStackFrames().

--- src/java/org/dellroad/stuff/pobj/PersistentObjectTransactionManager.java
+++ src/java/org/dellroad/stuff/pobj/PersistentObjectTransactionManager.java
@@ -561,7 +561,7 @@ public class PersistentObjectTransactionManager<T> extends AbstractBean implemen
     // Prepend the second exception's stack frames onto the first exception's stack frames
     void prependStackFrames(Throwable t, Throwable p, String description, int skip) {
         final StackTraceElement[] innerFrames = t.getStackTrace();
-        final StackTraceElement[] outerFrames = new Throwable().getStackTrace();
+        final StackTraceElement[] outerFrames = p.getStackTrace();
         final StackTraceElement[] frames = new StackTraceElement[innerFrames.length + 1 + Math.max(outerFrames.length - skip, 0)];
         System.arraycopy(innerFrames, 0, frames, 0, innerFrames.length);
         frames[innerFrames.length] = new StackTraceElement(this.getClass().getName(), "<placeholder>", description, -1);
