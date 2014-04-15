ba15fb7ad5eb71502f4f061bf85402d313a56082
archie.cobbs <archie.cobbs@3d3da37c-52f5-b908-f4a3-ab77ce6ea90f>
2014-04-15 15:34:06 +0000

    Add ByteUtil utility methods for encoding big endian 32 and 64 bit values.

--- src/java/org/jsimpledb/core/DoubleType.java
+++ src/java/org/jsimpledb/core/DoubleType.java
@@ -9,6 +9,7 @@ package org.jsimpledb.core;
 
 import org.dellroad.stuff.java.Primitive;
 import org.jsimpledb.util.ByteReader;
+import org.jsimpledb.util.ByteUtil;
 import org.jsimpledb.util.ByteWriter;
 
 /**
@@ -31,15 +32,7 @@ class DoubleType extends PrimitiveType<Double> {
 
     @Override
     public Double read(ByteReader reader) {
-        long bits =
-            ((long)reader.readByte() << 56)
-          | ((long)reader.readByte() << 48)
-          | ((long)reader.readByte() << 40)
-          | ((long)reader.readByte() << 32)
-          | ((long)reader.readByte() << 24)
-          | ((long)reader.readByte() << 16)
-          | ((long)reader.readByte() <<  8)
-          | ((long)reader.readByte());
+        long bits = ByteUtil.readLong(reader);
         bits ^= (bits & SIGN_BIT) == 0 ? NEG_XOR : POS_XOR;
         return Double.longBitsToDouble(bits);
     }
@@ -48,14 +41,7 @@ class DoubleType extends PrimitiveType<Double> {
     public void write(ByteWriter writer, Double value) {
         long bits = Double.doubleToLongBits(value);
         bits ^= (bits & SIGN_BIT) != 0 ? NEG_XOR : POS_XOR;
-        writer.writeByte((int)(bits >> 56));
-        writer.writeByte((int)(bits >> 48));
-        writer.writeByte((int)(bits >> 40));
-        writer.writeByte((int)(bits >> 32));
-        writer.writeByte((int)(bits >> 24));
-        writer.writeByte((int)(bits >> 16));
-        writer.writeByte((int)(bits >> 8));
-        writer.writeByte((int)bits);
+        ByteUtil.writeLong(writer, bits);
     }
 
     @Override
