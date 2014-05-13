6ad16c145de097aa4d201159a73d1c744285625e
archie.cobbs <archie.cobbs@3d3da37c-52f5-b908-f4a3-ab77ce6ea90f>
2014-05-13 18:24:32 +0000

    - Make SnapshotTransaction a public class - Add reset() method to Snapshot{J,}Transaction classes - Add JTransaction.resetValidationQueue() - XMLObjectSerializer:   - Return number of objects processed from read() and write()   - When importing, copy in via snapshot to avoid delete actions

--- src/java/org/jsimpledb/core/SnapshotTransaction.java
+++ src/java/org/jsimpledb/core/SnapshotTransaction.java
@@ -7,30 +7,108 @@
 
 package org.jsimpledb.core;
 
-class SnapshotTransaction extends Transaction {
+/**
+ * An initially empty, in-memory {@link Transaction} that persists indefinitely.
+ *
+ * <p>
+ * {@link SnapshotTransaction}s hold a "snapshot" some portion of the state of a {@link Transaction} for later use.
+ * Each {@link SnapshotTransaction}s contains its own set of "snapshot" objects that are copies of
+ * corresponding "real" database objects (as of the time they were copied).
+ * </p>
+ *
+ * @see Transaction#createSnapshotTransaction
+ */
+public class SnapshotTransaction extends Transaction {
 
     SnapshotTransaction(Transaction parent) {
         super(parent.db, new SnapshotKVTransaction(parent), parent.schema, parent.version);
     }
 
+    /**
+     * Delete all objects contained in this snapshot transaction and reset it back to its initial state.
+     *
+     * <p>
+     * It will contain schema meta-data but no objects.
+     * </p>
+     */
+    public void reset() {
+
+        // Sanity check
+        if (this.stale)
+            throw new StaleTransactionException(this);
+
+        // Delete all object and index keys
+        this.db.reset(this);
+    }
+
+    /**
+     * Commit this transaction.
+     *
+     * <p>
+     * {@link SnapshotTransaction}s do not support this method and will always throw {@link UnsupportedOperationException}.
+     * </p>
+     *
+     * @throws UnsupportedOperationException always
+     */
     @Override
     public void commit() {
         throw new UnsupportedOperationException("snapshot transaction");
     }
 
+    /**
+     * Roll back this transaction.
+     *
+     * <p>
+     * {@link SnapshotTransaction}s do not support this method and will always throw {@link UnsupportedOperationException}.
+     * </p>
+     *
+     * @throws UnsupportedOperationException always
+     */
     @Override
     public void rollback() {
         throw new UnsupportedOperationException("snapshot transaction");
     }
 
+    /**
+     * Mark this transaction for rollback only.
+     *
+     * <p>
+     * {@link SnapshotTransaction}s do not support this method and will always throw {@link UnsupportedOperationException}.
+     * </p>
+     *
+     * @throws UnsupportedOperationException always
+     */
     @Override
     public void setRollbackOnly() {
         throw new UnsupportedOperationException("snapshot transaction");
     }
 
+    /**
+     * Register a transaction callback to be invoked when this transaction completes.
+     *
+     * <p>
+     * {@link SnapshotTransaction}s do not support this method and will always throw {@link UnsupportedOperationException}.
+     * </p>
+     *
+     * @throws UnsupportedOperationException always
+     */
     @Override
     public void addCallback(Callback callback) {
         throw new UnsupportedOperationException("snapshot transaction");
+    }
+
+    /**
+     * Determine whether this transaction is still valid.
+     *
+     * <p>
+     * {@link SnapshotTransaction}s are always valid.
+     * </p>
+     *
+     * @return true always
+     */
+    @Override
+    public boolean isValid() {
+        return super.isValid();
     }
 }
 
