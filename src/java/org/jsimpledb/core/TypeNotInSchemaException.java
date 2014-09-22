ff943d4b53cf9b5743d6689f71c999f71ff6ff64
archie.cobbs <archie.cobbs@3d3da37c-52f5-b908-f4a3-ab77ce6ea90f>
2014-09-22 01:40:16 +0000

    Commit new class that was supposed to be part of r449.

---
+++ src/java/org/jsimpledb/core/TypeNotInSchemaException.java
@@ -0,0 +1,51 @@
+
+/*
+ * Copyright (C) 2014 Archie L. Cobbs. All rights reserved.
+ *
+ * $Id$
+ */
+
+package org.jsimpledb.core;
+
+/**
+ * Thrown when attempting to update a database object to a schema version in which the object's type is not defined.
+ *
+ * @see Transaction#updateSchemaVersion Transaction.updateSchemaVersion()
+ */
+@SuppressWarnings("serial")
+public class TypeNotInSchemaException extends DatabaseException {
+
+    private final ObjId id;
+    private final int version;
+
+    /**
+     * Constructor.
+     */
+    public TypeNotInSchemaException(ObjId id, int version) {
+        this(id, version, "no object type with storage ID " + id.getStorageId() + " exists in database schema version " + version);
+    }
+
+    /**
+     * Constructor.
+     */
+    public TypeNotInSchemaException(ObjId id, int version, String message) {
+        super(message);
+        this.id = id;
+        this.version = version;
+    }
+
+    /**
+     * Get the {@link ObjId} of the object that could not be upgraded to its unrecognized type.
+     */
+    public ObjId getObjId() {
+        return this.id;
+    }
+
+    /**
+     * Get the schema version which does not contain the object's type.
+     */
+    public int getSchemaVersion() {
+        return this.version;
+    }
+}
+
