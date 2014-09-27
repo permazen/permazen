003b19a0df52851e873fc6e356e9d1c9c3a2a60e
archie.cobbs <archie.cobbs@3d3da37c-52f5-b908-f4a3-ab77ce6ea90f>
2014-09-27 21:27:20 +0000

    Disallow copying between objects having different types.

---
+++ trunk/src/test/org/jsimpledb/core/CopyToWrongTypeTest.java
@@ -0,0 +1,56 @@
+
+/*
+ * Copyright (C) 2014 Archie L. Cobbs. All rights reserved.
+ *
+ * $Id$
+ */
+
+package org.jsimpledb.core;
+
+import java.io.ByteArrayInputStream;
+import java.util.UUID;
+
+import org.jsimpledb.TestSupport;
+import org.jsimpledb.kv.simple.SimpleKVDatabase;
+import org.jsimpledb.schema.SchemaModel;
+import org.testng.annotations.Test;
+
+public class CopyToWrongTypeTest extends TestSupport {
+
+    @Test
+    public void testCopyToWrongType() throws Exception {
+
+        // Setup database
+
+        final SimpleKVDatabase kvstore = new SimpleKVDatabase();
+
+        final SchemaModel schema = SchemaModel.fromXML(new ByteArrayInputStream((
+            "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+          + "<Schema>\n"
+          + "  <Object name=\"Foo\" storageId=\"1\">\n"
+          + "    <SimpleField name=\"long\" type=\"long\" storageId=\"2\"/>\n"
+          + "  </Object>\n"
+          + "  <Object name=\"Bar\" storageId=\"3\">\n"
+          + "    <SimpleField name=\"uuid\" type=\"java.util.UUID\" storageId=\"4\"/>\n"
+          + "  </Object>\n"
+          + "</Schema>\n"
+          ).getBytes("UTF-8")));
+
+        final Database db = new Database(kvstore);
+
+        Transaction tx = db.createTransaction(schema, 1, true);
+
+        final ObjId foo = tx.create(1);
+        tx.writeSimpleField(foo, 2, 1234L, false);
+        final ObjId bar = tx.create(3);
+        tx.writeSimpleField(bar, 4, UUID.randomUUID(), false);
+
+        try {
+            tx.copy(bar, foo, tx);
+            assert false : "copied foo to bar!";
+        } catch (IllegalArgumentException e) {
+            // expected
+        }
+    }
+}
+
