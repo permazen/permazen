a7196a3ae5e3a4ea215f1e6ac4839da725cfc0bb
archie.cobbs <archie.cobbs@3d3da37c-52f5-b908-f4a3-ab77ce6ea90f>
2014-10-10 21:23:52 +0000

    Remove unused field.

--- src/java/org/jsimpledb/core/ObjTypeStorageInfo.java
+++ src/java/org/jsimpledb/core/ObjTypeStorageInfo.java
@@ -7,22 +7,10 @@
 
 package org.jsimpledb.core;
 
-import java.util.SortedMap;
-import java.util.TreeMap;
-
 class ObjTypeStorageInfo extends StorageInfo {
-
-    final TreeMap<Integer, FieldStorageInfo> fields = new TreeMap<>();
 
     ObjTypeStorageInfo(ObjType objType) {
         super(objType.storageId);
-    }
-
-    /**
-     * Get the fields associated with this instance.
-     */
-    public SortedMap<Integer, FieldStorageInfo> getFields() {
-        return this.fields;
     }
 
     @Override
