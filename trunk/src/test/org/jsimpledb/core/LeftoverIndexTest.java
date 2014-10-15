2ce4b442a0e2e3b6024c7e64006f3dbfde6133e5
archie.cobbs <archie.cobbs@3d3da37c-52f5-b908-f4a3-ab77ce6ea90f>
2014-10-15 20:17:06 +0000

    Add test displaying bug where index entries from deleted complex fields were not being cleaned up after schema update.

---
+++ trunk/src/test/org/jsimpledb/core/LeftoverIndexTest.java
@@ -0,0 +1,70 @@
+
+/*
+ * Copyright (C) 2014 Archie L. Cobbs. All rights reserved.
+ *
+ * $Id$
+ */
+
+package org.jsimpledb.core;
+
+import java.io.ByteArrayInputStream;
+import java.util.NavigableSet;
+
+import org.jsimpledb.TestSupport;
+import org.jsimpledb.kv.simple.SimpleKVDatabase;
+import org.jsimpledb.schema.SchemaModel;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LeftoverIndexTest extends TestSupport {
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testLeftoverIndex() throws Exception {
+
+        final SimpleKVDatabase kvstore = new SimpleKVDatabase();
+
+        final SchemaModel schemaA = SchemaModel.fromXML(new ByteArrayInputStream((
+            "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+          + "<Schema formatVersion=\"1\">\n"
+          + "  <ObjectType name=\"Foo\" storageId=\"1\">\n"
+          + "    <SetField name=\"set\" storageId=\"2\">\n"
+          + "      <SimpleField type=\"int\" storageId=\"3\" indexed=\"true\"/>\n"
+          + "    </SetField>"
+          + "  </ObjectType>\n"
+          + "</Schema>\n"
+          ).getBytes("UTF-8")));
+
+        final SchemaModel schemaB = SchemaModel.fromXML(new ByteArrayInputStream((
+            "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+          + "<Schema formatVersion=\"1\">\n"
+          + "  <ObjectType name=\"Foo\" storageId=\"1\">\n"
+          + "  </ObjectType>\n"
+          + "</Schema>\n"
+          ).getBytes("UTF-8")));
+
+        final Database db = new Database(kvstore);
+
+    // Version 1
+
+        Transaction tx = db.createTransaction(schemaA, 1, true);
+
+        final ObjId id1 = tx.create(1);
+        NavigableSet<Integer> set = (NavigableSet<Integer>)tx.readSetField(id1, 2, true);
+        set.add(123);
+
+        Assert.assertEquals(set, buildSet(123));
+        Assert.assertEquals(tx.querySimpleField(3), buildMap(123, buildSet(id1)));
+
+        tx.commit();
+
+    // Version 2
+
+        tx = db.createTransaction(schemaB, 2, true);
+
+        Assert.assertEquals(tx.querySimpleField(3), buildMap());
+
+        tx.commit();
+    }
+}
+
