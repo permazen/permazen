417af2732d26defc366bb56dba3c0a5f2082f937
Archie L. Cobbs <archiecobbs@users.noreply.github.com>
2014-11-07 19:38:34 +0000

    Add ObjectComparator.

---
+++ src/java/org/dellroad/stuff/java/ObjectComparator.java
@@ -0,0 +1,99 @@
+
+/*
+ * Copyright (C) 2011 Archie L. Cobbs. All rights reserved.
+ *
+ * $Id$
+ */
+
+package org.dellroad.stuff.java;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+
+import java.util.Comparator;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * A {@link Comparator} that creates a stable, total ordering of all Java objects.
+ *
+ * <p>
+ * Instances are compared first using {@linkplain System#identityHashCode identity hash codes}, and then using
+ * an internally generated unique identifier; identifiers are indexed using weak keys to avoid memory leaks.
+ * </p>
+ *
+ * <p>
+ * Note: while each instance creates a stable sort ordering, distinct instances of this class may sort objects differently.
+ * </p>
+ *
+ * <p>
+ * Null values are supported and always sort last. Instances of this class are thread safe.
+ * </p>
+ *
+ * <p>
+ * This class requires <a href="https://github.com/google/guava">Google Guava</a>.
+ * </p>
+ */
+public class ObjectComparator implements Comparator<Object> {
+
+    private final AtomicLong nextId = new AtomicLong();
+    private final LoadingCache<Object, Long> idMap;
+
+    /**
+     * Default constructor.
+     */
+    public ObjectComparator() {
+        this(CacheBuilder.newBuilder());
+    }
+
+    /**
+     * Constructor.
+     *
+     * @param concurrencyLevel guides the allowed concurrency among update operations; used as a hint for internal sizing
+     * @throws IllegalArgumentException if {@code concurrencyLevel} is zero or less
+     * @see CacheBuilder#concurrencyLevel
+     */
+    public ObjectComparator(int concurrencyLevel) {
+        this(CacheBuilder.newBuilder().concurrencyLevel(concurrencyLevel));
+    }
+
+    /**
+     * Internal constructor.
+     *
+     * @param cacheBuilder object unique ID cache builder
+     * @throws NullPointerException if {@code cacheBuilder} is null
+     */
+    protected ObjectComparator(CacheBuilder cacheBuilder) {
+        this.idMap = cacheBuilder.weakKeys().<Object, Long>build(new CacheLoader<Object, Long>() {
+            @Override
+            public Long load(Object obj) {
+                return ObjectComparator.this.nextId.getAndIncrement();
+            }
+        });
+    }
+
+    @Override
+    public int compare(Object obj1, Object obj2) {
+
+        // Handle nulls
+        final boolean null1 = obj1 == null;
+        final boolean null2 = obj2 == null;
+        if (null1 || null2)
+            return null1 && null2 ? 0 : null1 ? 1 : -1;
+
+        // Compare hash values
+        final int hash1 = System.identityHashCode(obj1);
+        final int hash2 = System.identityHashCode(obj2);
+        if (hash1 < hash2)
+            return -1;
+        if (hash1 > hash2)
+            return 1;
+
+        // Compare unique serial numbers (it should be very rare that we get to this point)
+        final long id1 = this.idMap.getUnchecked(obj1);
+        final long id2 = this.idMap.getUnchecked(obj2);
+        assert id1 != id2;
+        return Long.compare(id1, id2);
+    }
+}
+
