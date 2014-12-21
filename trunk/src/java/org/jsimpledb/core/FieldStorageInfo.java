80e1ce8eb91e8e10865148e33c37617bb89a3924
archie.cobbs <archie.cobbs@3d3da37c-52f5-b908-f4a3-ab77ce6ea90f>
2014-12-21 16:51:01 +0000

    Move FieldStorageInfo.superFieldStorageId to SimpleFieldStorageInfo where it belongs.

--- trunk/src/java/org/jsimpledb/core/FieldStorageInfo.java
+++ trunk/src/java/org/jsimpledb/core/FieldStorageInfo.java
@@ -9,35 +9,15 @@ package org.jsimpledb.core;
 
 abstract class FieldStorageInfo extends StorageInfo {
 
-    final int superFieldStorageId;
-
-    FieldStorageInfo(Field<?> field, int superFieldStorageId) {
+    FieldStorageInfo(Field<?> field) {
         super(field.storageId);
-        this.superFieldStorageId = superFieldStorageId;
     }
 
     /**
      * Get whether the associated field is a sub-field.
      */
     public boolean isSubField() {
-        return this.superFieldStorageId != 0;
-    }
-
-// Object
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this)
-            return true;
-        if (!super.equals(obj))
-            return false;
-        final FieldStorageInfo that = (FieldStorageInfo)obj;
-        return this.superFieldStorageId == that.superFieldStorageId;
-    }
-
-    @Override
-    public int hashCode() {
-        return super.hashCode() ^ this.superFieldStorageId;
+        return false;
     }
 }
 
