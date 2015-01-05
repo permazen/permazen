4ac676e8b69f777313cd6b547d3d49a6cc2b9d51
archie.cobbs <archie.cobbs@3d3da37c-52f5-b908-f4a3-ab77ce6ea90f>
2015-01-05 20:47:58 +0000

    Add tests that were forgotten in recent commits.

---
+++ trunk/src/test/org/jsimpledb/StorageIdTest.java
@@ -0,0 +1,122 @@
+
+/*
+ * Copyright (C) 2014 Archie L. Cobbs. All rights reserved.
+ *
+ * $Id$
+ */
+
+package org.jsimpledb;
+
+import java.io.ByteArrayInputStream;
+import java.util.List;
+import java.util.NavigableMap;
+import java.util.SortedSet;
+
+import org.jsimpledb.annotation.JCompositeIndex;
+import org.jsimpledb.annotation.JField;
+import org.jsimpledb.annotation.JMapField;
+import org.jsimpledb.annotation.JSimpleClass;
+import org.jsimpledb.kv.simple.SimpleKVDatabase;
+import org.jsimpledb.schema.SchemaModel;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class StorageIdTest extends TestSupport {
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testStorageIds() throws Exception {
+
+        final SimpleKVDatabase kvstore = new SimpleKVDatabase();
+
+        final DefaultStorageIdGenerator storageIdGenerator = new DefaultStorageIdGenerator();
+        final int fooId = storageIdGenerator.generateClassStorageId(Foo.class, "Foo");
+        final int stringId = storageIdGenerator.generateFieldStorageId(Foo.class.getMethod("getString"), "string");
+        final int friendId = storageIdGenerator.generateFieldStorageId(Friendly.class.getMethod("getFriend"), "friend");
+        final int setId = storageIdGenerator.generateFieldStorageId(Foo.class.getMethod("getSet"), "set");
+        final int setElementId = storageIdGenerator.generateSetElementStorageId(Foo.class.getMethod("getSet"), "set");
+        final int listId = storageIdGenerator.generateFieldStorageId(Foo.class.getMethod("getList"), "list");
+        final int listElementId = storageIdGenerator.generateListElementStorageId(Foo.class.getMethod("getList"), "list");
+        final int mapId = storageIdGenerator.generateFieldStorageId(Foo.class.getMethod("getMap"), "map");
+        final int mapKeyId = storageIdGenerator.generateMapKeyStorageId(Foo.class.getMethod("getMap"), "map");
+        final int mapValueId = storageIdGenerator.generateMapValueStorageId(Foo.class.getMethod("getMap"), "map");
+        final int indexId = storageIdGenerator.generateCompositeIndexStorageId(Foo.class,
+          "index", new int[] { stringId, friendId });
+
+        final SchemaModel expected = SchemaModel.fromXML(new ByteArrayInputStream((
+            "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+          + "<Schema formatVersion=\"2\">\n"
+          + "  <ObjectType name=\"Foo\" storageId=\"" + fooId + "\">\n"
+          + "    <SimpleField name=\"string\" type=\"java.lang.String\" storageId=\"" + stringId + "\"/>\n"
+          + "    <ReferenceField name=\"friend\" storageId=\"" + friendId + "\">\n"
+          + "      <ObjectTypes>\n"
+          + "        <ObjectType storageId=\"" + fooId + "\"/>\n"
+          + "      </ObjectTypes>\n"
+          + "    </ReferenceField>\n"
+          + "    <SetField name=\"set\" storageId=\"" + setId + "\">\n"
+          + "      <SimpleField type=\"java.lang.Integer\" storageId=\"" + setElementId + "\"/>\n"
+          + "    </SetField>"
+          + "    <ListField name=\"list\" storageId=\"" + listId + "\">\n"
+          + "      <SimpleField type=\"java.lang.Float\" storageId=\"" + listElementId + "\"/>\n"
+          + "    </ListField>"
+          + "    <MapField name=\"map\" storageId=\"" + mapId + "\">\n"
+          + "      <ReferenceField storageId=\"" + mapKeyId + "\">\n"
+          + "        <ObjectTypes>\n"
+          + "          <ObjectType storageId=\"" + fooId + "\"/>\n"
+          + "        </ObjectTypes>\n"
+          + "      </ReferenceField>\n"
+          + "      <SimpleField type=\"double\" storageId=\"" + mapValueId + "\"/>\n"
+          + "    </MapField>"
+          + "    <CompositeIndex name=\"index\" storageId=\"" + indexId + "\">\n"
+          + "      <IndexedField storageId=\"" + stringId + "\"/>\n"
+          + "      <IndexedField storageId=\"" + friendId + "\"/>\n"
+          + "    </CompositeIndex>\n"
+          + "  </ObjectType>\n"
+          + "</Schema>\n"
+          ).getBytes("UTF-8")));
+
+        final SchemaModel actual = new JSimpleDB(Foo.class).getSchemaModel();
+
+        Assert.assertEquals(actual, expected);
+    }
+
+// Model Classes
+
+    public interface Friendly {
+
+        Foo getFriend();
+        void setFriend(Foo friend);
+    }
+
+    @JSimpleClass(compositeIndexes = @JCompositeIndex(name = "index", fields = { "string", "friend" }))
+    public abstract static class Foo implements JObject, Friendly {
+
+        public abstract String getString();
+        public abstract void setString(String string);
+
+        public abstract SortedSet<Integer> getSet();
+
+        public abstract List<Float> getList();
+
+        @JMapField(value = @JField(type = "double"))
+        public abstract NavigableMap<Friendly, Double> getMap();
+
+        // Not a bean getter method, should be ignored
+        public int someMethod(int y) {
+            return y + 1;
+        }
+
+        // Not an abstract method, should be ignored
+        public int getFoo() {
+            return 0;
+        }
+        public void setFoo(int foo) {
+        }
+
+        @Override
+        public String toString() {
+            return "Foo@" + this.getObjId();
+        }
+    }
+}
+
---
+++ trunk/src/test/org/jsimpledb/core/FieldTypesFilterTest.java
@@ -0,0 +1,108 @@
+
+/*
+ * Copyright (C) 2014 Archie L. Cobbs. All rights reserved.
+ *
+ * $Id$
+ */
+
+package org.jsimpledb.core;
+
+import com.google.common.collect.Lists;
+
+import java.io.ByteArrayInputStream;
+import java.util.Arrays;
+
+import org.jsimpledb.TestSupport;
+import org.jsimpledb.kv.KVPairIterator;
+import org.jsimpledb.kv.KVTransaction;
+import org.jsimpledb.kv.KeyRange;
+import org.jsimpledb.kv.KeyRanges;
+import org.jsimpledb.kv.simple.SimpleKVDatabase;
+import org.jsimpledb.schema.SchemaModel;
+import org.jsimpledb.tuple.Tuple2;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class FieldTypesFilterTest extends TestSupport {
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void testFieldTypesFilter() throws Exception {
+
+        final SimpleKVDatabase kvdb = new SimpleKVDatabase();
+        final Database db = new Database(kvdb);
+
+        KVTransaction kvt = kvdb.createTransaction();
+
+        kvt.put(b("aaaa0302"), b(""));
+        kvt.put(b("aaaa0303"), b(""));
+
+        kvt.put(b("aaaa0401"), b(""));
+        kvt.put(b("aaaa0402"), b(""));
+        kvt.put(b("aaaa0404"), b(""));
+
+        kvt.put(b("aaaa0501"), b(""));
+        kvt.put(b("aaaa0502"), b(""));
+        kvt.put(b("aaaa0503"), b(""));
+        kvt.put(b("aaaa0504"), b(""));
+
+        final UnsignedIntType uintType = new UnsignedIntType();
+
+        final FieldTypesFilter filter1 = new FieldTypesFilter(b("aaaa"), uintType, uintType);
+        final KVPairIterator i1 = new KVPairIterator(kvt, KeyRange.forPrefix(b("aaaa04")), filter1, false);
+        Assert.assertEquals(Lists.newArrayList(i1), Arrays.asList(
+            kv("aaaa0401"),
+            kv("aaaa0402"),
+            kv("aaaa0404")
+        ));
+
+        final FieldTypesFilter filter2 = filter1.filter(1, new KeyRanges(b("02"), b("04")));
+        final KVPairIterator i2 = new KVPairIterator(kvt, KeyRange.forPrefix(b("aaaa04")), filter2, false);
+        Assert.assertEquals(Lists.newArrayList(i2), Arrays.asList(
+            kv("aaaa0402")
+        ));
+
+        kvt.rollback();
+
+        final SchemaModel schema1 = SchemaModel.fromXML(new ByteArrayInputStream((
+            "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+          + "<Schema formatVersion=\"1\">\n"
+          + "  <ObjectType name=\"Foo\" storageId=\"10\">\n"
+          + "    <SimpleField name=\"s\" type=\"java.lang.String\" storageId=\"11\" indexed=\"true\"/>\n"
+          + "  </ObjectType>\n"
+          + "</Schema>\n"
+          ).getBytes("UTF-8")));
+
+        Transaction tx = db.createTransaction(schema1, 1, true);
+
+        final ObjId id1 = new ObjId("0a11111111111111");
+        final ObjId id2 = new ObjId("0a22222222222222");
+        final ObjId id3 = new ObjId("0a33333333333333");
+
+        tx.create(id1);
+        tx.create(id2);
+        tx.create(id3);
+
+        tx.writeSimpleField(id1, 11, "aaa", true);
+        tx.writeSimpleField(id2, 11, "aaa", true);
+        tx.writeSimpleField(id3, 11, "bbb", true);
+
+        final CoreIndex<String, ObjId> index = (CoreIndex<String, ObjId>)tx.querySimpleField(11);
+        TestSupport.checkSet(index.asSet(), buildSet(
+          new Tuple2<String, ObjId>("aaa", id1),
+          new Tuple2<String, ObjId>("aaa", id2),
+          new Tuple2<String, ObjId>("bbb", id3)));
+        TestSupport.checkMap(index.asMap(), buildMap(
+          "aaa", buildSet(id1, id2),
+          "bbb", buildSet(id3)));
+
+        final CoreIndex<String, ObjId> index2 = index.filter(1, new KeyRanges(b("0a20"), b("0a30")));
+        TestSupport.checkSet(index2.asSet(), buildSet(
+          new Tuple2<String, ObjId>("aaa", id2)));
+        TestSupport.checkMap(index2.asMap(), buildMap(
+          "aaa", buildSet(id2)));
+
+        tx.commit();
+    }
+}
+
