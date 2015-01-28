1ba1600ae396d104021a121334dcdc1d4beafbe8
archie.cobbs <archie.cobbs@3d3da37c-52f5-b908-f4a3-ab77ce6ea90f>
2015-01-28 17:26:43 +0000

    Add missing component of r643: also remove the redundant JTransaction parameter from JField.setValue().

--- trunk/src/java/org/jsimpledb/parse/expr/JSimpleFieldValue.java
+++ trunk/src/java/org/jsimpledb/parse/expr/JSimpleFieldValue.java
@@ -9,7 +9,6 @@ package org.jsimpledb.parse.expr;
 
 import org.jsimpledb.JObject;
 import org.jsimpledb.JSimpleField;
-import org.jsimpledb.JTransaction;
 import org.jsimpledb.parse.ParseSession;
 
 /**
@@ -33,7 +32,7 @@ public class JSimpleFieldValue extends JFieldValue implements LValue {
     public void set(ParseSession session, Value value) {
         final Object obj = value.get(session);
         try {
-            ((JSimpleField)this.jfield).setValue(JTransaction.getCurrent(), this.jobj, obj);
+            ((JSimpleField)this.jfield).setValue(this.jobj, obj);
         } catch (IllegalArgumentException e) {
             throw new EvalException("invalid " + AbstractValue.describeType(obj) + " for field `" + this.jfield.getName() + "'"
               + (e.getMessage() != null ? ": " + e.getMessage() : ""), e);
