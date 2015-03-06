cbf92b238ca730fd736e14fffc237f2c564ca884
archie.cobbs <archie.cobbs@3d3da37c-52f5-b908-f4a3-ab77ce6ea90f>
2015-03-06 22:56:44 +0000

    Use LONGBLOB for the value column in MySQL.

--- trunk/src/java/org/jsimpledb/kv/sql/createTable-mysql.sql
+++ trunk/src/java/org/jsimpledb/kv/sql/createTable-mysql.sql
@@ -1,7 +1,11 @@
 -- $Id$
 
 CREATE TABLE IF NOT EXISTS `KV` (
+    -- MySQL InnoDB indexes are limited to 767 bytes
+    -- You can go up to 3072 bytes if you set innodb_large_prefix 
+    -- See: http://dev.mysql.com/doc/refman/5.5/en/innodb-restrictions.html
+    -- Also recommended: innodb_file_per_table
    `kv_key` VARBINARY(767) NOT NULL,
-   `kv_value` VARBINARY(767) NOT NULL,
+   `kv_value` LONGBLOB NOT NULL,
    PRIMARY KEY(`kv_key`)
 ) ENGINE=InnoDB default charset=utf8 collate=utf8_bin
