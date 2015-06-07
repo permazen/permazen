9c5551113983224abae55b56ca3b9efe8dc85c32
Archie L. Cobbs <archie.cobbs@gmail.com>
2015-06-07 14:59:20 -0500

    Javadoc tweak.

--- src/java/org/jsimpledb/core/package-info.java
+++ src/java/org/jsimpledb/core/package-info.java
@@ -7,12 +7,14 @@
  * Core classes for the JSimpleDB Java persistence layer.
  *
  * <p>
- * The classes in this package implement the core JSimpleDB engine. This layer can function
+ * The classes in this package implement the JSimpleDB core API. The core API layer can function
  * entirely independently from the annotation-based {@link org.jsimpledb.JSimpleDB} Java object model layer;
- * the latter is implemented on top of these classes.
+ * the latter is implemented on top of these classes. In turn, the core API layer is implemented on top
+ * of the {@link org.jsimpledb.kv key/value store} layer.
  * </p>
  *
  * @see org.jsimpledb
+ * @see org.jsimpledb.kv
  * @see <a href="http://jsimpledb.googlecode.com/">The JSimpleDB Project</a>
  */
 package org.jsimpledb.core;
