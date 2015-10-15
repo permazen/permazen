71c37138100ab024321712350b56993872dce81e
Archie L. Cobbs <archie.cobbs@gmail.com>
2015-10-15 11:08:48 -0500

    Add new unit test exposing a bug that needs to be fixed.
    When a session variable holds a reference to a JObject, if we don't
    refresh the JObject in each new transaction, then we get an exception
    when trying to access the object because it's old transaction is closed.

---
+++ src/test/org/jsimpledb/parse/expr/DetachedObjectTest.java
@@ -0,0 +1,75 @@
+
+/*
+ * Copyright (C) 2015 Archie L. Cobbs. All rights reserved.
+ */
+
+package org.jsimpledb.parse.expr;
+
+import org.jsimpledb.BasicTest;
+import org.jsimpledb.JObject;
+import org.jsimpledb.JSimpleDB;
+import org.jsimpledb.JTransaction;
+import org.jsimpledb.TestSupport;
+import org.jsimpledb.annotation.JSimpleClass;
+import org.jsimpledb.parse.ParseContext;
+import org.jsimpledb.parse.ParseSession;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class DetachedObjectTest extends TestSupport {
+
+    private ParseSession session;
+
+    @BeforeClass
+    public void setup() throws Exception {
+        final JSimpleDB jdb = BasicTest.getJSimpleDB(Person.class);
+        this.session = new ParseSession(jdb);
+        this.session.setSchemaVersion(1);
+        this.session.setAllowNewSchema(true);
+        this.session.registerStandardFunctions();
+    }
+
+    @Test
+    public void testDetachedObject() {
+
+        // Create object and assign it to variable $x
+        boolean success = this.session.performParseSessionAction(new ParseSession.TransactionalAction() {
+            @Override
+            public void run(ParseSession session) {
+
+                // Create "Fred"
+                final JTransaction jtx = session.getJTransaction();
+                final Person fred = jtx.create(Person.class);
+                fred.setName("Fred");
+
+                // Assign $x = fred
+                new ExprParser().parse(session, new ParseContext("$x = @" + fred.getObjId()), false).evaluate(session);
+            }
+        });
+        Assert.assertTrue(success);
+
+        // Now dereference $x in a new transaction
+        success = this.session.performParseSessionAction(new ParseSession.TransactionalAction() {
+            @Override
+            public void run(ParseSession session) {
+
+                // Get $x.name
+                final Object name = new ExprParser().parse(session,
+                  new ParseContext("$x.name"), false).evaluate(session).get(session);
+                Assert.assertEquals(name, "Fred");
+            }
+        });
+        Assert.assertTrue(success);
+    }
+
+// Model classes
+
+    @JSimpleClass
+    public abstract static class Person implements JObject {
+
+        public abstract String getName();
+        public abstract void setName(String name);
+    }
+}
+
