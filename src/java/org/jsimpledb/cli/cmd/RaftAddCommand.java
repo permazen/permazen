b1846c23c9edd23e9eab9a2633bec8eb5b29912f
Archie L. Cobbs <archie.cobbs@gmail.com>
2015-10-29 17:14:39 -0500

    Fix typos in command help.

--- src/java/org/jsimpledb/cli/cmd/RaftAddCommand.java
+++ src/java/org/jsimpledb/cli/cmd/RaftAddCommand.java
@@ -30,9 +30,9 @@ public class RaftAddCommand extends AbstractTransactionRaftCommand {
         return "This command adds a new node to the Raft cluster. The new node's identity and address (in the form"
           + " IPAddress[:port]) must be provided. This command can also be used to change the known address of a node"
           + " already in the cluster as recorded in the cluster configuration (the node must also be restarted with the"
-          + "  new address if its address actually needs to change). This command may be run from any cluster node."
+          + " new address if its address actually needs to change). This command may be run from any cluster node."
           + "\n\nThis command is also used to initialize an unconfigured node. This creates a new cluster with the"
-          + "local node as the first and only member. In this usage, the local node's identity and address must be given;"
+          + " local node as the first and only member. In this usage, the local node's identity and address must be given;"
           + " for unconfigured nodes, this is the only allowed usage of this command.";
     }
 
