8a5fda29980f162fe44d725ce2b87ee81c30cf69
Archie L. Cobbs <archie.cobbs@gmail.com>
2016-07-07 22:13:50 -0500

    Refactor EvalCommand to parse EvalAction's to allow error access.

--- jsimpledb-cli/src/main/java/org/jsimpledb/cli/cmd/EvalCommand.java
+++ jsimpledb-cli/src/main/java/org/jsimpledb/cli/cmd/EvalCommand.java
@@ -39,29 +39,61 @@ public class EvalCommand extends AbstractCommand {
     }
 
     @Override
-    public CliSession.Action getAction(CliSession session, ParseContext ctx, boolean complete, Map<String, Object> params) {
+    public EvalAction getAction(CliSession session, ParseContext ctx, boolean complete, Map<String, Object> params) {
         final Node expr = (Node)params.get("expr");
         final boolean force = params.containsKey("force");
-        return new CliSession.TransactionalAction() {
-            @Override
-            public void run(CliSession session) throws Exception {
-                final PrintWriter writer = session.getWriter();
-                final Value value;
-                final Object result;
-                try {
-                    result = (value = expr.evaluate(session)).get(session);
-                } catch (EvalException e) {
-                    if (!force && session.getMode().hasCoreAPI())
-                        session.getTransaction().setRollbackOnly();
-                    writer.println(session.getErrorMessagePrefix() + e.getMessage());
-                    if (session.isVerbose())
-                        e.printStackTrace(writer);
-                    return;
-                }
-                if (value != Value.NO_VALUE)
-                    writer.println(result);
+        return new EvalAction(expr, force);
+    }
+
+// EvalAction
+
+    /**
+     * Special {@link CliSession.Action} used by the {@link EvalCommand} allowing access to the
+     * {@link EvalException} that occurred, if any.
+     *
+     * <p>
+     * This class is needed because {@link EvalCommand} catches and handles {@link EvalException}s
+     * thrown during expression evaluation itself, swallowing them, making it appear as if the
+     * command were always successful.
+     */
+    public static class EvalAction implements CliSession.TransactionalAction {
+
+        private final Node expr;
+        private final boolean force;
+        private EvalException evalException;
+
+        private EvalAction(Node expr, boolean force) {
+            this.expr = expr;
+            this.force = force;
+        }
+
+        @Override
+        public void run(CliSession session) throws Exception {
+            final PrintWriter writer = session.getWriter();
+            final Value value;
+            final Object result;
+            try {
+                result = (value = expr.evaluate(session)).get(session);
+            } catch (EvalException e) {
+                this.evalException = e;
+                if (!force && session.getMode().hasCoreAPI())
+                    session.getTransaction().setRollbackOnly();
+                writer.println(session.getErrorMessagePrefix() + e.getMessage());
+                if (session.isVerbose())
+                    e.printStackTrace(writer);
+                return;
             }
-        };
+            if (value != Value.NO_VALUE)
+                writer.println(result);
+        }
+
+        /**
+         * Get the {@link EvalException} that occurred when evaluating the expression, if any.
+         *
+         * @return exception thrown during evaluation, or null if evaluation was successful
+         */
+        public EvalException getEvalException() {
+            return this.evalException;
+        }
     }
 }
-
