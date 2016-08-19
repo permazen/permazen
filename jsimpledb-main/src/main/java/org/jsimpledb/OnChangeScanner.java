05e0af6e508a364113b27322ded7f6c33422225c
Archie L. Cobbs <archie.cobbs@gmail.com>
2016-08-19 11:28:12 -0500

    Fix incorrect indentation.

--- jsimpledb-main/src/main/java/org/jsimpledb/OnChangeScanner.java
+++ jsimpledb-main/src/main/java/org/jsimpledb/OnChangeScanner.java
@@ -255,7 +255,7 @@ class OnChangeScanner<T> extends AnnotationScanner<T, OnChange> {
             default:
                 throw new RuntimeException("internal error");
             }
-    }
+        }
 
     // SimpleFieldChangeListener
 
