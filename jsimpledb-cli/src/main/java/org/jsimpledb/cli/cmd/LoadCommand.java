85b4f2baa69fe7ae94492cd2cfd76f95a8423991
Archie L. Cobbs <archie.cobbs@gmail.com>
2016-09-07 13:11:44 -0500

    Add `-R' flag to the load CLI command.

--- jsimpledb-cli/src/main/java/org/jsimpledb/cli/cmd/LoadCommand.java
+++ jsimpledb-cli/src/main/java/org/jsimpledb/cli/cmd/LoadCommand.java
@@ -18,7 +18,7 @@ import org.jsimpledb.util.ParseContext;
 public class LoadCommand extends AbstractCommand {
 
     public LoadCommand() {
-        super("load file.xml:file");
+        super("load -R:reset file.xml:file");
     }
 
     @Override
@@ -28,8 +28,8 @@ public class LoadCommand extends AbstractCommand {
 
     @Override
     public String getHelpDetail() {
-        return "Imports objects from an XML file created previously via `save'. Does not remove any objects already"
-          + " in the database.";
+        return "Imports objects from an XML file created previously via `save'. Does NOT remove any objects already"
+          + " in the database unless the `-R' flag is given, in which case the database is completely wiped first.";
     }
 
     @Override
@@ -39,17 +39,25 @@ public class LoadCommand extends AbstractCommand {
 
     @Override
     public CliSession.Action getAction(CliSession session, ParseContext ctx, boolean complete, Map<String, Object> params) {
+        final boolean reset = params.containsKey("reset");
         final File file = (File)params.get("file.xml");
-
-        // Return import action
         return new CliSession.TransactionalAction() {
             @Override
             public void run(CliSession session) throws Exception {
-                final int count;
-                try (BufferedInputStream input = new BufferedInputStream(new FileInputStream(file))) {
-                    count = new XMLObjectSerializer(session.getTransaction()).read(input);
+                final Transaction tx = session.getTransaction();
+                if (reset) {
+                    int deleteCount = 0;
+                    for (ObjId id : tx.getAll()) {
+                        tx.delete(id);
+                        deleteCount++;
+                    }
+                    session.getWriter().println("Deleted " + deleteCount + " object(s)");
                 }
-                session.getWriter().println("Read " + count + " objects from `" + file + "'");
+                final int readCount;
+                try (BufferedInputStream input = new BufferedInputStream(new FileInputStream(file))) {
+                    readCount = new XMLObjectSerializer(session.getTransaction()).read(input);
+                }
+                session.getWriter().println("Read " + readCount + " object(s) from `" + file + "'");
             }
         };
     }
