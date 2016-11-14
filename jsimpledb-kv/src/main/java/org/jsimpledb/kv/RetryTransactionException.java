f6b3e6bd03bbd0f961348b3d8d5c8990f4dcab7d
Archie L. Cobbs <archie.cobbs@gmail.com>
2016-11-14 16:00:27 -0600

    Javadoc++

--- jsimpledb-kv/src/main/java/org/jsimpledb/kv/RetryTransactionException.java
+++ jsimpledb-kv/src/main/java/org/jsimpledb/kv/RetryTransactionException.java
@@ -7,7 +7,16 @@ package org.jsimpledb.kv;
 
 /**
  * Exception thrown when an attempt to access a {@link KVTransaction} results
- * in a conflict requiring that the transaction be restarted.
+ * in a conflict or other condition requiring that the transaction be retried.
+ *
+ * <p>
+ * <b>Note:</b> although it is usually the case, a {@link RetryTransactionException}
+ * does <b>not</b> necessarily mean that the transaction failed. In some situations
+ * (e.g., involving failed network communication) it may not be possible to determine
+ * whether a transaction succeeded or not, and this situation is indicated by throwing a
+ * {@link RetryTransactionException}. Whether and how often such a "false negative" can
+ * occur is a function of the {@link KVDatabase} implementation being used; ideally, all
+ * transactions are idempotent and therefore correct in either case.
  */
 @SuppressWarnings("serial")
 public class RetryTransactionException extends KVTransactionException {
