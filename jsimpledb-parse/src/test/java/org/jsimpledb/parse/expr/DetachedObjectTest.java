8bd71247fb254e05c5a1e2c213a692290fd263e6
Archie L. Cobbs <archie.cobbs@gmail.com>
2017-02-04 13:32:37 -0600

    Add new parsing unit test (disabled for now).

--- jsimpledb-parse/src/test/java/org/jsimpledb/parse/expr/DetachedObjectTest.java
+++ jsimpledb-parse/src/test/java/org/jsimpledb/parse/expr/DetachedObjectTest.java
@@ -5,6 +5,8 @@
 
 package org.jsimpledb.parse.expr;
 
+import java.util.function.Consumer;
+
 import org.jsimpledb.JObject;
 import org.jsimpledb.JSimpleDB;
 import org.jsimpledb.JTransaction;
@@ -31,30 +33,64 @@ public class DetachedObjectTest extends TestSupport {
     }
 
     @Test
-    public void testDetachedObject() {
+    public void testDetachedObject1() {
 
-        // Create object and assign it to variable $x
-        boolean success = this.session.performParseSessionAction((TestAction)session2 -> {
+        // Create "Fred" object and assign it to variable $fred
+        this.doAction(session2 -> {
 
             // Create "Fred"
             final JTransaction jtx = session2.getJTransaction();
             final Person fred = jtx.create(Person.class);
             fred.setName("Fred");
 
-            // Assign $x = fred
-            new ExprParser().parse(session2, new ParseContext("$x = @" + fred.getObjId()), false).evaluate(session2);
+            // Assign $fred = fred
+            this.doParse(session2, "$fred = @" + fred.getObjId());
         });
-        Assert.assertTrue(success);
 
-        // Now dereference $x in a new transaction
-        success = this.session.performParseSessionAction((TestAction)session2 -> {
+        // Now dereference $fred in a new transaction
+        final Object name = this.doParse("$fred.name");
+        Assert.assertEquals(name, "Fred");
+    }
 
-            // Get $x.name
-            final Object name = new ExprParser().parse(session2,
-              new ParseContext("$x.name"), false).evaluate(session2).get(session2);
-            Assert.assertEquals(name, "Fred");
+    //@Test     FIXME
+    public void testDetachedObject2() {
+
+        // Create "Fred" object and assign it to variable $fred
+        this.doAction(session2 -> {
+
+            // Create "Fred"
+            final JTransaction jtx = session2.getJTransaction();
+            final Person fred = jtx.create(Person.class);
+            fred.setName("Fred");
+
+            // Assign $fred = fred
+            this.doParse(session2, "$fred = @" + fred.getObjId());
         });
+
+        // Assign $set1 = Collections.singleton(fred)
+        this.doParse("$set1 = java.util.Collections.singleton($fred)");
+
+        // Assign $set2 = Collections.singleton(fred)
+        this.doParse("$set2 = java.util.Collections.singleton($fred)");
+
+        // Sets should be equal
+        final Object result = this.doParse("$set1.equals($set2)");
+        Assert.assertEquals(result, Boolean.TRUE);
+    }
+
+    private void doAction(Consumer<ParseSession> action) {
+        final boolean success = this.session.performParseSessionAction((TestAction)session2 -> action.accept(session2));
         Assert.assertTrue(success);
+    }
+
+    private Object doParse(String command) {
+        final Object[] result = new Object[1];
+        this.doAction(session2 -> result[0] = this.doParse(session2, command));
+        return result[0];
+    }
+
+    private Object doParse(ParseSession session2, String command) {
+        return new ExprParser().parse(session2, new ParseContext(command), false).evaluate(session2).get(session2);
     }
 
     private interface TestAction extends ParseSession.Action, Session.TransactionalAction {
