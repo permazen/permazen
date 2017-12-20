47f12d2b1407e447701375960c640c62dfba988a
Archie L. Cobbs <archie.cobbs@gmail.com>
2017-12-20 08:40:01 -0600

    Javadoc++

--- permazen-coreapi/src/main/java/io/permazen/core/InconsistentDatabaseException.java
+++ permazen-coreapi/src/main/java/io/permazen/core/InconsistentDatabaseException.java
@@ -8,6 +8,12 @@ package io.permazen.core;
 /**
  * Thrown when inconsistent content is detected in a {@link Database} indicating a corrupted or invalid database,
  * or a buggy underlying key-value store.
+ *
+ * <p>
+ * If you get one of these exceptions, you may consider running the {@code jsck} command in the Permazen CLI,
+ * which checks for database consistency problems and (optionally) attempts to repair them.
+ *
+ * @see io.permazen.jsck.Jsck
  */
 @SuppressWarnings("serial")
 public class InconsistentDatabaseException extends DatabaseException {
