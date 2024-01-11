d6807d94bbb93e4b9147359dc14e9940b837bed4
Archie L. Cobbs <archie.cobbs@gmail.com>
2024-01-11 14:58:31 -0600

    Javadoc fixes.

--- permazen-main/src/main/java/io/permazen/UntypedPermazenObject.java
+++ permazen-main/src/main/java/io/permazen/UntypedPermazenObject.java
@@ -11,26 +11,25 @@ import io.permazen.core.TypeNotInSchemaException;
  * Represents a {@link PermazenObject} for which no Java model type is defined in the instance's associated schema.
  *
  * <p>
- * Instances of this class are used to represent objects with a type that is defined in some database schema
- * other than the current one. This situation can occur when a new schema drops a previously defined Java model type
- * of which objects still exist in the database. If encountered, such objects are represented by instances of
+ * Instances of this class are used to represent objects with a type that is defined in some other database schema
+ * but not in the current one. This situation can occur when a new schema drops a previously defined Java model type
+ * when there are objects still existing in the database. If encountered, such objects are represented by instances of
  * this class.
  *
  * <p>
- * These objects are still fully accessible, but they must be accessed via introspection using the
- * {@link PermazenTransaction} field access methods, with the {@code upgradeVersion} parameter set to false
- * (to prevent a {@link TypeNotInSchemaException}).
+ * These objects are still fully accessible, but they must be accessed via introspection using the {@link PermazenTransaction}
+ * field access methods, with the {@code migrateSchema} parameter set to false (to prevent a {@link TypeNotInSchemaException}).
  *
  * <p>
- * For example, suppose a schema update replaces a field referencing {@code Account} with a simple {@link String}
- * field containing the account ID. Then the corresponding schema migration might look like this:
+ * For example, suppose a schema update removes the {@code Account} class and replaces fields referencing {@code Account}
+ * objects with a simple {@link String} account ID field. Then the corresponding schema migration might look like this:
  * <pre>
  *      &#64;OnSchemaChange
  *      private void applySchemaChanges(Map&lt;String, Object&gt; oldValues) {
  *          if (oldValues.containsKey("account")) {                         // was replaced with "accountId"
  *              final PermazenObject acct = (PermazenObject)oldValues.get("account");     // acct has type UntypedPermazenObject
  *              final PermazenTransaction ptx = this.getTransaction();
- *              final String acctId = (String)ptx.readSimpleField(acct.getObjId(), 12345, false);
+ *              final String acctId = (String)ptx.readSimpleField(acct.getObjId(), "accountId", false);
  *              this.setAccountId(acctId);
  *          }
  *          // ...etc
