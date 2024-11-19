permazen/permazen

commit e47c679ea5d44e7b1b0144c3e92a61c9fdcf0d6c

Quote stuff "like this" instead of `like this'.

permazen-kv-mysql/src/main/java/io/permazen/kv/mysql/MySQLKVDatabase.java
@@ -110,7 +110,7 @@ public class MySQLKVDatabase extends SQLKVDatabase {
      */
     @Override
     public String quote(String name) {
-        return "`" + name + "`";
+        return "\"" + name + "\"";
     }
 
     /**

commit 0bd0ef190919c96795f39617f354415d18b7e409

Fix MySQL quoting breakage caused by commit e47c679ea5.

permazen-kv-mysql/src/main/java/io/permazen/kv/mysql/MySQLKVDatabase.java
@@ -110,7 +110,7 @@ public class MySQLKVDatabase extends SQLKVDatabase {
      */
     @Override
     public String quote(String name) {
-        return "\"" + name + "\"";
+        return "`" + name + "`";
     }
 
     /**